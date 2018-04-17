5d735dfafb527e7a2c7031f79d6b1c7a6d3e99b3
Thomas Dane <tjdane@gmail.com>
2018-04-17 21:02:12 +0100

    Add selenium

--- OciusFormTests/OciusFormTests.cs
+++ OciusFormTests/OciusFormTests.cs
@@ -1,18 +1,92 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
-namespace Tests
+namespace SeleniumTests
 {
-    public class Tests
+    [TestFixture]
+    public class UntitledTestCase
     {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
         [SetUp]
-        public void Setup()
+        public void SetupTest()
         {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.katalon.com/";
+            verificationErrors = new StringBuilder();
         }
-
-        [Test]
-        public void Test1()
+        
+        [TearDown]
+        public void TeardownTest()
         {
-            Assert.Pass();
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void TheUntitledTestCaseTest()
+        {
+            driver.Navigate().GoToUrl("https://ocius.com.au/bluebottle-usv-brochure/");
+            driver.FindElement(By.Name("full-name")).SendKeys("Ocius Test");
+            driver.FindElement(By.Name("contact-email")).SendKeys("tom@ocius.com.au");
+            driver.FindElement(By.Name("reason-interest")).SendKeys("Weekly test of \"Request a Brochure\" form.");
+            driver.FindElement(By.XPath("//input[@value='Send']")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
         }
     }
 }
